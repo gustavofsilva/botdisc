954ffdb8d041e2fe89c53747122455a94df5d1dc
luizg <luiz.g.f.s@hotmail.com>
2025-03-13 02:14:20 -0300

    cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
-const path = require("path");
+const cors = require('cors');
 const express = require("express");
 const fs = require("fs");
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+app.use(cors({
+    origin: '*', 
+}));
 
 const client = new Client({
     intents: [
